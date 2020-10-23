6cafd4e076268cf5ebd2013091b257822f77564b
Nicholas Monath <nmonath@cs.umass.edu>
2020-10-22 23:35:40 -0400

    remove path names

--- pv/disambiguation/assignee/qc/top100.py
+++ pv/disambiguation/assignee/qc/top100.py
@@ -29,31 +29,31 @@ def main():
     top100new = new_disambig_data.nlargest(100, 'cluster_size')
 
     get_entity_name_new = """
-        SELECT DISTINCT(organization) from tmp_assignee_disambiguation_granted INNER JOIN rawassignee ON rawassignee.uuid =tmp_assignee_disambiguation_granted.uuid WHERE disambiguated_id="%s" LIMIT 10;
+        SELECT DISTINCT(organization) from rawassignee INNER JOIN tmp_assignee_disambiguation_granted ON rawassignee.uuid=tmp_assignee_disambiguation_granted.uuid WHERE disambiguated_id="%s" LIMIT 10;
         """
 
     get_entity_name_old = """
             SELECT DISTINCT(organization) from rawassignee WHERE assignee_id="%s" LIMIT 10;
             """
     from tqdm import tqdm
-    with open('top_100_old.txt', 'w') as fout:
-        for idx, val in tqdm(top100old.iterrows()):
-            this_q = (get_entity_name_old + '') % val[1]
-            with granted_db.cursor() as cursor:
-                cursor.execute(this_q)
-                results = pd.DataFrame(cursor.fetchall(), columns=[i[0] for i in cursor.description])
-                names = [x for x in results['organization']]
-                name_str = '; '.join(names)
-                fout.write('%s\t%s\t%s\n' % (val[0], val[1], name_str))
-                cursor.close()
+    # with open('assignee_top_100_old.txt', 'w') as fout:
+    #     for idx, val in tqdm(top100old.iterrows()):
+    #         this_q = (get_entity_name_old + '') % val[1]
+    #         with granted_db.cursor() as cursor:
+    #             cursor.execute(this_q)
+    #             results = pd.DataFrame(cursor.fetchall(), columns=[i[0] for i in cursor.description])
+    #             names = [x for x in results['organization']]
+    #             name_str = '; '.join(names)
+    #             fout.write('%s\t%s\t%s\n' % (val[0], val[1], name_str))
+    #             cursor.close()
 
-    with open('top_100_new.txt', 'w') as fout:
+    with open('assignee_top_100_new.txt', 'w') as fout:
         for idx, val in tqdm(top100new.iterrows()):
             this_q = (get_entity_name_new + '') % val[1]
             with granted_db.cursor() as cursor:
                 cursor.execute(this_q)
                 results = pd.DataFrame(cursor.fetchall(), columns=[i[0] for i in cursor.description])
-                names = [x for x in results['organization']]
+                names = [x[1][0] for x in results.iterrows()]
                 name_str = '; '.join(names)
                 fout.write('%s\t%s\t%s\n' % (val[0], val[1], name_str))
                 cursor.close()
