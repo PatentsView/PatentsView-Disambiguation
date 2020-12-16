d7187e557b3f3660170d9d9d98a8fecc701e4938
Sarvothaman Madhavan <smadhavan@air.org>
2020-12-15 19:12:33 -0500

    Updated config subsection for database connection

--- pv/disambiguation/util/db.py
+++ pv/disambiguation/util/db.py
@@ -5,10 +5,10 @@ def granted_table(config):
     return mysql.connector.connect(host=config['DATABASE']['host'],
                                              user=config['DATABASE']['username'],
                                              password=config['DATABASE']['password'],
-                                             database=config['DATABASE']['granted_table'])
+                                             database=config['DATABASE']['granted_patent_database'])
 
 def pregranted_table(config):
     return mysql.connector.connect(host=config['DATABASE']['host'],
                                              user=config['DATABASE']['username'],
                                              password=config['DATABASE']['password'],
-                                             database=config['DATABASE']['pregranted_table'])
+                                             database=config['DISAMBIGUATION']['pregrant_database'])
