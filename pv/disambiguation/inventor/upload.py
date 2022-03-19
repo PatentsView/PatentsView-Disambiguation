3306ae8113313d550daa143678e1753403d8897f
Sarvo Madhavan <smadhavan@air.org>
2022-03-19 11:42:55 -0400

    Moved commit before DDL and allowed primary key to exist

--- pv/disambiguation/inventor/upload.py
+++ pv/disambiguation/inventor/upload.py
@@ -39,9 +39,15 @@ def load_target_from_source(config, pairs, target='granted_patent_database'):
             ['("%s", "%s")' % x for x in pairs[sidx:eidx]])
         # logging.log_first_n(logging.INFO, '%s', 1, sql)
         g_cursor.execute(sql)
-    g_cursor.execute(
-        'alter table {table_name} add primary key (uuid)'.format(table_name=config['INVENTOR_UPLOAD']['target_table']))
     cnx_g.commit()
+    try:
+        g_cursor.execute(
+            'alter table {table_name} add primary key (uuid)'.format(
+                table_name=config['INVENTOR_UPLOAD']['target_table']))
+    except mysql.connector.errors.ProgrammingError as e:
+        from mysql.connector import errorcode
+        if not e.errno == errorcode.ER_MULTIPLE_PRI_KEY:
+            raise
     cnx_g.close()
 
 
