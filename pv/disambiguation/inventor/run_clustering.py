7ec0b89441557265a44688ff430088ca894cde53
Beth Anne Card <bcard@air.org>
2023-01-11 11:51:12 -0700

    rerunning job-0

--- pv/disambiguation/inventor/run_clustering.py
+++ pv/disambiguation/inventor/run_clustering.py
@@ -241,24 +241,25 @@ def run_clustering(config):
     logging.info('%s chunk_size', int(config['inventor']['chunk_size']))
 
     # chunk all of the data by canopy
-    # chunks = [[] for _ in range(num_chunks)]
-    # for idx, c in enumerate(all_canopies_sorted):
-    #     chunks[idx % num_chunks].append(c)
-    # pool = mp.Pool(int(config['inventor']['parallelism']))
+    chunks = [[] for _ in range(num_chunks)]
+    for idx, c in enumerate(all_canopies_sorted):
+        chunks[idx % num_chunks].append(c)
+    pool = mp.Pool(int(config['inventor']['parallelism']))
     # for x in range(0, num_chunks):
-    # for x in [0]:
-    #    logging.log(logging.INFO, 'Chunk {x}'.format(x=x))
-    #    run_batch(config, chunks[x], outdir, x, 'job-%s' % x )
-    #
-    # argument_list = [(config, chunks[x], outdir, x, 'job-%s' % x) for x in range(0, num_chunks)]
-    # dev_null = [
-    #     n for n in pool.starmap(
-    #         run_batch, argument_list)
-    # ]
+    for x in [0]:
+       logging.log(logging.INFO, 'Chunk {x}'.format(x=x))
+       run_batch(config, chunks[x], outdir, x, 'job-%s' % x )
+
+    argument_list = [(config, chunks[x], outdir, x, 'job-%s' % x) for x in range(0, num_chunks)]
+    dev_null = [
+        n for n in pool.starmap(
+            run_batch, argument_list)
+    ]
+    breakpoint()
     # chunk 0 will write out the meta data and singleton information
-    # logging.info('Saving chunk to canopy map')
-    # with open(outdir + '/chunk2canopies.pkl', 'wb') as fout:
-    #     pickle.dump([chunks, list(singletons)], fout)
+    logging.info('Saving chunk to canopy map')
+    with open(outdir + '/chunk2canopies.pkl', 'wb') as fout:
+        pickle.dump([chunks, list(singletons)], fout)
 
     logging.info('Running singletons!!')
     num_singleton_chunks = max(1, int(len(singletons) / int(config['inventor']['chunk_size'])))
@@ -272,12 +273,6 @@ def run_clustering(config):
     for x in range(0, num_singleton_chunks):
         run_singletons(config, singleton_chunks[x], outdir, 'singleton-job-%s' % x)
 
-    # pool = mp.Pool()
-    # argument_list = [(config, singleton_chunks[x], outdir, 'singleton-job-%s' % x) for x in range(0, num_singleton_chunks)]
-    # dev_null = [
-    #     n for n in pool.starmap(
-    #         run_singletons, argument_list)
-    # ]
 
 
 def main(argv):
