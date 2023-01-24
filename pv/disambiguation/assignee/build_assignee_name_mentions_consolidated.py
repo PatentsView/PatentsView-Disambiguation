d8d49618d36d54c185f3f2eb60bc1d530f56fedf
Beth Anne Card <bcard@air.org>
2023-01-24 12:39:01 -0700

    updating the location query

--- pv/disambiguation/assignee/build_assignee_name_mentions_consolidated.py
+++ pv/disambiguation/assignee/build_assignee_name_mentions_consolidated.py
@@ -49,6 +49,7 @@ def generate_assignee_records_from_sql(config, ignore_filters, source='granted_p
     FROM
         {db}.rawassignee ra
         left join {db}.rawlocation rl on rl.id=ra.rawlocation_id
+        left join patent.location l on rl.location_id=l.id
         {filter}
         """.format(
         id_field=incremental_components.get('id_field'),
