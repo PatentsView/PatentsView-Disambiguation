0ce92f8c5e795f76decb2ca33d1af13bb3616bd7
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2025-04-06 18:41:02 -0400

    getting rid of end date dash

--- pv/QA/AssigneeDisambiguationPipelineTester.py
+++ pv/QA/AssigneeDisambiguationPipelineTester.py
@@ -1,6 +1,7 @@
 import os
 import pickle
-from datetime import datetime, timezone
+from datetime import datetime
+
 from pv.QA.testing_utils import get_file_modified_time
 
 
@@ -13,10 +14,11 @@ class AssigneeDisambiguationPipelineTester():
         # Local imports
         from pv.disambiguation.core import AssigneeNameMention
         # Config setup
-        end_date = self.config["DATES"]["END_DATE"]["END_DATE_DASH"]
-        path = f"{self.config['BASE_PATH']['assignee']}".format(end_date=end_date) + \
+        end_date = self.config["DATES"]["END_DATE"]
+        path = f"{self.config['BASE_PATH']['assignee']}".format(end_date=end_date,
+                                                                data_root=self.config['FOLDERS']['data_root']) + \
                self.config['BUILD_ASSIGNEE_NAME_MENTIONS']['feature_out']
-        print(path)
+
         # Variables to be used in testing
         name_mention_attributes = ['uuid', 'name_hash', 'name_features', 'mention_ids', 'record_ids',
                                    'location_strings', 'canopies', 'unique_exact_strings',
@@ -27,13 +29,7 @@ class AssigneeDisambiguationPipelineTester():
         records_last_modified_date = get_file_modified_time(pickle_file_path)
         canopies_last_modified_date = get_file_modified_time(canopy_file_path)
         # loading
-        airflow_run_date = airflow_run_date.replace(tzinfo=None)
         records = pickle.load(open(pickle_file_path, 'rb'))
-        print("=============================================")
-        print(records_last_modified_date)
-        print(canopies_last_modified_date)
-        print(airflow_run_date)
-        print("=============================================")
 
         ## Assertions go here
         # Make sure files exist. (This can go away if we add test for canopies)
