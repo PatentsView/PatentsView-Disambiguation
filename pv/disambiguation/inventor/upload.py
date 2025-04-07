cce74c2839facf3dae468cfb939941ede5d45a52
Graham Chickering <40693743+gchickering21@users.noreply.github.com>
2025-04-07 14:50:38 -0400

    getting the duplicates removed

--- pv/disambiguation/inventor/upload.py
+++ pv/disambiguation/inventor/upload.py
@@ -34,26 +34,26 @@ def load_target_from_source(config, pairs, target='granted_patent_database'):
     batch_size = 100000
     inventor_disambig_table= config["DISAMBIG_TABLES"]["INVENTOR"]
     offsets = [x for x in range(0, len(pairs), batch_size)]
-    #print("INSERT INTO {table_name} (uuid, inventor_id) VALUES .... ".format(table_name=config['INVENTOR_UPLOAD']['target_table']) )
-    for idx in tqdm(range(len(offsets)), 'adding %s' % target, total=len(offsets),mininterval=(len(offsets)/100)):
-        sidx = offsets[idx]
-        eidx = min(len(pairs), offsets[idx] + batch_size)
-        sql = "INSERT INTO {table_name} (uuid, inventor_id) VALUES ".format(
-            table_name=inventor_disambig_table) + ', '.join(['("%s", "%s")' % x for x in pairs[sidx:eidx]])
-        # logging.log_first_n(logging.INFO, '%s', 1, sql)
-        g_cursor.execute(sql)
-    cnx_g.commit()
+    print("INSERT INTO {table_name} (uuid, inventor_id) VALUES .... ".format(table_name=inventor_disambig_table) )
+    # for idx in tqdm(range(len(offsets)), 'adding %s' % target, total=len(offsets),mininterval=(len(offsets)/100)):
+    #     sidx = offsets[idx]
+    #     eidx = min(len(pairs), offsets[idx] + batch_size)
+    #     sql = "INSERT INTO {table_name} (uuid, inventor_id) VALUES ".format(
+    #         table_name=inventor_disambig_table) + ', '.join(['("%s", "%s")' % x for x in pairs[sidx:eidx]])
+    #     # logging.log_first_n(logging.INFO, '%s', 1, sql)
+    #     g_cursor.execute(sql)
+    # cnx_g.commit()
     try:
         g_cursor.execute(
             '''
             DELETE FROM {table_name}
-            WHERE uuid NOT IN (
-                SELECT MIN(uuid) AS uuid
+            WHERE rowid NOT IN (
+                SELECT MIN(rowid)
                 FROM {table_name}
                 GROUP BY uuid
             )
             '''.format(
-                table_name=config['INVENTOR_UPLOAD']['target_table']
+                table_name=inventor_disambig_table
             )
         )
         # Fetch the number of rows affected
